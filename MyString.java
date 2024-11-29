150d1220030ace1fa6b449adf0573f69ecca892d
Tomer Elankry <tomerelankry@Tomers-MacBook-Pro.local>
2024-11-29 18:38:36 +0200

    completed

--- MyString.java
+++ MyString.java
@@ -1,30 +1,56 @@
 public class MyString {
     public static void main(String[] args) {
-        System.out.println("Testing lowercase:");
+        /*System.out.println("Testing lowercase:");
         System.out.println("UnHappy : " + lowerCase("UnHappy"));
         System.out.println("This costs 15 Sheksls : " + lowerCase("This costs 15 Sheksls"));
         System.out.println("TLV : " + lowerCase("TLV"));
-        System.out.println("lowercase : " + lowerCase("lowercase"));
+        System.out.println("lowercase : " + lowerCase("lowercase"));*/
 
         System.out.println("Testing contains:");
-        System.out.println(contains("unhappy", "happy")); // true
-        System.out.println(contains("happy", "unhappy")); // false
-        System.out.println(contains("historical", "story")); // false
-        System.out.println(contains("psychology", "psycho")); // true
-        System.out.println(contains("personality", "son")); // true
-        System.out.println(contains("personality", "dad")); // false
-        System.out.println(contains("resignation", "sign")); // true
+        System.out.println(contains("baba yaga", "baba")); // true
+        System.out.println(contains("baba yaga", "")); // false
+        System.out.println(contains("baba yaga", "John Wick is the baba yaga")); // false
+        System.out.println(contains("baba yaga", "Yaga")); // true
+        System.out.println(contains("baba yaga", "babayaga")); // true
+        //System.out.println(contains("personality", "dad")); // false
+        
+        //System.out.println(contains("baba yaga", "babayaga"));// true
     }
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String newS="";
+        for(int i=0;i<str.length();i++)
+        {
+            if(str.charAt(i)>=65 && str.charAt(i)<=90)
+            {
+                newS+=(char)(str.charAt(i)+32);
+            }
+            else newS+=str.charAt(i);
+        }
+        return newS;
     }
-
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if(str2.length()>str1.length()) return false;
+        if(str2.length()==0) return true;
+        //str1 = lowerCase(str1);
+        //str2 = lowerCase(str2);
+        int count = 0;
+
+        for(int i=0;i<str1.length();i++)
+        {
+            if(str1.charAt(i)==str2.charAt(0))
+            {
+                for(int j = 0;j<str2.length();j++)
+                {
+                    if(str1.charAt(i+j)!=str2.charAt(j)) break;
+                    else count++;
+                }
+                if(count==str2.length()) return true;
+            }
+        }
+    
         return false;
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,5 +1,34 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+       int n = Integer.parseInt(args[0]);
+       System.out.println("Prime numbers up to "+n+":");
+       int count=0;
+       int i = 2;
+       double per=0;
+       while (i<=n)
+       {
+          if(isPrime(i))
+          { 
+            count++;
+            System.out.println(i);
+          }
+          i++;
+       }
+       per= ((double)count/n)*100;
+
+       System.out.println("There are "+count+" primes between "+ "2 and "+n+" ("+(int)per+"% are primes)");
     }
+
+    public static boolean isPrime(int s)
+    {
+        int j = 2;
+        if(s==2) return true;
+        while (j<s)
+        {
+            if(s%j==0) return false;
+            j++;
+        }
+        return true;
+    }
+    
 }
